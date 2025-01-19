07c0caa78d5d7b57137609bea8f3c3c6f4e69969
Anastasia Chepelenko <bondarenkoanastasia854@gmail.com>
2025-01-19 20:04:40 +0300

    Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            String[] actions = {"+", "-", "/", "*"};
+            String[] regexActions = {"\\+", "-", "/", "\\*"};
+            Scanner scn = new Scanner(System.in);
+            System.out.print("Введите выражение: ");
+            String exp = scn.nextLine();
+
+            int actionIndex = -1;
+            for (int i = 0; i < actions.length; i++) {
+                if (exp.contains(actions[i])) {
+                    actionIndex = i;
+                    break;
+                }
+            }
+            if (actionIndex == -1) {
+                System.out.println("Некорректное выражение");
+                return;
+            }
+            String[] data = exp.split(regexActions[actionIndex]);
+            if (data.length > 2) {
+                throw new Exception("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            }
+            int a;
+            int b;
+            try {
+                a = Integer.parseInt(data[0]);
+                b = Integer.parseInt(data[1]);
+                if (a < 1 | a > 10 | b < 1 | b > 10) {
+                    throw new Exception("Вводите цифры от 1 до 10");
+                }
+            } catch (Exception e) {
+                throw new Exception("Один из операндов не является числом. Проверьте пробелы в выражении");
+            }
+
+            int result;
+            switch (actions[actionIndex]) {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                default:
+                    result = a / b;
+                    break;
+            }
+            System.out.println("Ответ: " + result);
+
+        }
+    }
+}
